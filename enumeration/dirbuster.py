5fc581e7d16d7baebf338075a0b41a84cacf0911
aleksa <zabumaphu@gmail.com>
2024-10-24 20:57:04 -0400

    dirbusting done.

--- enumeration/dirbuster.py
+++ enumeration/dirbuster.py
@@ -4,5 +4,21 @@
 Author: Aleksa Zatezalo
 Version: 0.0
 Date: 
-Description: 
+Description: A simple implementation of dirbuster.
 """
+
+import requests
+
+def dirb(url, path):
+    try:
+        path = url + "/" + path
+        r = requests.get(path)
+        if r.status_code == 200:
+            print("\033[93m" + f"[!] {path} Found" + "\033[0m")
+    except:
+       pass
+
+def wordListScan(url, wordlistPath):
+    with open(wordlistPath) as file:
+        while line := file.readline():
+            dirb(url, line.rstrip())
