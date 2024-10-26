c868d2a266057b8317f17f6b23ac14d9f84525cf
aleksa <zabumaphu@gmail.com>
2024-10-26 19:46:30 -0400

    Port scanned Async

--- enumeration/portScan.py
+++ enumeration/portScan.py
@@ -4,47 +4,34 @@
 Author: Aleksa Zatezalo
 Version: 1.0
 Date: October 2024
-Description: A very basic port scanner that prints information to standard outout.
+Description: An async tcp port scanner.
 """
 
 import sys
 import socket
+import asyncio
 
-def scanPort(host, port):
-    """
-    Takes one string host, and an int port and attempts to conntect.
-    Information will be printed to standard output.
-    """
+async def test_port_number(host, port, timeout=3):
+    coro = asyncio.open_connection(host, port)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)  # Set a connection timeout
-
-    result = sock.connect_ex((host, port)) 
-    sock.close()
-    return result
-
-def scanPortsMinMax(host, start_port, end_port):
-    """
-    Takes a string representing a url called host, and two integers called start_port and end_port.
-    It scans the ports on host from start_port to end_port.
-    """
-    ports = []
-    print(f"Scanning ports on {host}...")
-    for port in range(start_port, end_port + 1):
-        if (scanPort(host, port) == 0):
-            ports.append(port)   
+    try:
+        _, writer = await asyncio.wait_for(coro, timeout)
+        writer.close()
+        return True
+    except asyncio.TimeoutError:
+        return False
     
-    return ports
+async def main(host, ports):
+    print(f'Scanning {host}...')
+    coros = [test_port_number(host, port) for port in ports]
+    results = await asyncio.gather(*coros)
+    for port, result in zip(ports, results):
+        if result :
+            print(f'> {host}:{port} [OPEN]')
 
-def scanPortArray(host, portArray):
-    """
-    Takes a string representing a url called host, and two integers called start_port and end_port.
-    It scans the ports on host from start_port to end_port.
-    """
-    ports = []
-    print(f"Scanning ports on {host}...")
-    for port in portArray:
-        if (scanPort(host, port) == 0):
-            ports.append(port)   
-    
-    return ports
+
+# define a host and ports to scan
+host = 'python.org'
+ports = range(1, 1024)
+# start the asyncio program
+asyncio.run(main(host, ports))
