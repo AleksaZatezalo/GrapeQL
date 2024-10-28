7afe2db0e21b1e7530cc1516d8f4bafc7a33df97
aleksa <zabumaphu@gmail.com>
2024-10-27 22:31:43 -0400

    More files made

---
+++ enumeration/doEnum.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+Author: Aleksa Zatezalo
+Version: 1.0
+Date: October 2024
+Description: An basic web enumeration tool for GrapeQL.
+"""
+
+def printDirBust(results):
+    """
+    """
+
+    pass
+
+def printPortScan(results):
+    """
+    """
+
+    pass
+
+def promptDirBust():
+    """
+    """
+    
+    pass
+
+def promptPortScan():
+    """
+    """
+    
+    pass
+
+def executeDirBust():
+    """
+    """
+    
+    pass
+
+def executePortScan():
+    """
+    """
+    
+    pass
