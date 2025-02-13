7e534e6388365b13a990340bc8f759540480fe01
aleksa <zabumaphu@gmail.com>
2025-02-12 19:52:35 -0500

    Vine created

--- grapeql/vine.py
+++ grapeql/vine.py
@@ -4,3 +4,11 @@ Version: 1.0
 Date: February 2025
 Description: Enumeration script for GraphQL endpoints. Takes an IP and returns a list of endpoints with introspection enabled.
 """
+
+import asyncio
+import asyncio
+import aiohttp
+from grapePrint import grapePrint
+
+class vine():
+    pass
