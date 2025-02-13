629e84b330e057963b85b05668e7e035946d47eb
aleksa <zabumaphu@gmail.com>
2025-02-12 19:51:50 -0500

    Vine created

---
+++ grapeql/vine.py
@@ -0,0 +1,6 @@
+"""
+Author: Aleksa Zatezalo
+Version: 1.0
+Date: February 2025
+Description: Enumeration script for GraphQL endpoints. Takes an IP and returns a list of endpoints with introspection enabled.
+"""
