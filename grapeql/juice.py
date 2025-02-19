f25c29f3cf674c94c3abbb22820d81c9cf0198c0
aleksa <zabumaphu@gmail.com>
2025-02-19 00:36:01 -0500

    Juice created

---
+++ grapeql/juice.py
@@ -0,0 +1,11 @@
+"""
+Author: Aleksa Zatezalo
+Version: 1.0
+Date: February 2025
+Description: Module to test for command injection, sql injections, and other injection attacks. 
+"""
+
+class juice():
+
+    def __init__():
+        pass
